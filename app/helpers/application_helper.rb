f782f1c3351fbd01edeea2117ed8fa73c3224320
ovinix <dev.shvvolodymyr@hotmail.com>
2016-02-18 16:38:52 +0200

    Add a full_title helper and a get_superclass helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+
+	# Returns the full title on a per-page basis.
+	def full_title (page_title = '')
+		base_title = "Ruby on Rails Tutorial Sample App"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
+
+	# Returns the full class -> superclass structure of an object.
+	def get_superclass(obj=nil)
+		result = []
+		obj = obj.class unless obj.class == Class
+		if obj != NilClass
+	 		result << obj
+	 		result << get_superclass(obj.superclass)
+	 	end
+		return result.join(" ").split
+	end
 end
