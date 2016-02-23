44ab3238494ebab365fcd1ab961442dc16659211
ovinix <dev.shvvolodymyr@hotmail.com>
2016-02-23 15:57:06 +0200

    Flash implemented test (exercise 7.3)

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -22,6 +22,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   																						password_confirmation: "password" }
   	end
   	assert_template 'users/show'
+  	assert_not flash.empty?
   end
 
   test "invalid signup information" do
