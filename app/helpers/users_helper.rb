5c4ff66ce2cc7d9f00c9022ec91ceb1a21223ae3
ovinix <dev.shvvolodymyr@hotmail.com>
2016-02-29 21:54:34 +0200

    Fixed gravatar size error

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
   # Returns the Gravatar for the given user.
   def gravatar_for( user, opt = { size: 80 } )
   	gravatar_id = Digest::MD5::hexdigest( user.email.downcase )
-  	size = opt[size]
+  	size = opt[:size]
   	gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
   	image_tag( gravatar_url, alt: user.name, class: "gravatar" )
   end
