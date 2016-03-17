13df1ba80ebf0bfd816f0188232f292078d66d7d
ovinix <dev.shvvolodymyr@hotmail.com>
2016-03-17 19:58:45 +0200

    No 'flash' bug fixed

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -6,7 +6,7 @@ class MicropostsController < ApplicationController
     @micropost = current_user.microposts.build(micropost_params)
     
     if @micropost.save
-      flash[:success] = "Micropost created!"     
+      flash.now[:success] = "Micropost created!"     
       @micropost = current_user.microposts.build
     end
     
